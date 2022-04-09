2914f902c6b1460cc57b195b2de3685cf712f014
Matthew Bauer <mjbauer95@gmail.com>
2022-04-08 23:05:32 -0500

    Don’t hardcode /build
    Without linux sandboxing, the builds don’t get the /build/ directory.
    We can just use the current working directory nix provides.

--- ghc-nix/compile-hs.nix
+++ ghc-nix/compile-hs.nix
@@ -4,13 +4,13 @@ with import <nixpkgs> {};
 
 runCommand "compile-${ moduleName }.hs" {}
   ''
-  mkdir /build/build-results
+  mkdir build-results
   cp "${hs-path}" src.hs
   ${builtins.storePath ghc} -c src.hs \
     -package-db ${builtins.storePath package-db} \
     ${ args } \
-    -odir /build/build-results \
-    -hidir /build/build-results \
+    -odir build-results \
+    -hidir build-results \
     ${ lib.concatMapStringsSep " " ( dep: "-i${dep}" ) dependencies }
-  mv /build/build-results $out
+  mv build-results $out
   ''
